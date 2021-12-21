0a917109e5f2e8fe50c528d905a4cca610b84c23
Muriellehxh <dmui49@gmail.com>
2021-12-20 22:58:34 -0500

    mat

--- matrix.py
+++ matrix.py
@@ -2,7 +2,6 @@
 Chapitre 11.2
 """
 
-
 import numbers
 import copy
 import collections
@@ -10,141 +9,170 @@ import collections.abc
 
 
 class Matrix:
-	"""
-	Matrice numérique stockée en tableau 1D en format rangée-major.
+    """
+    Matrice numérique stockée en tableau 1D en format rangée-major.
 
-	:param height: La hauteur (nb de rangées)
-	:param width: La largeur (nb de colonnes)
-	:param data: Si une liste, alors les données elles-mêmes (affectées, pas copiées). Si un nombre, alors la valeur de remplissage
-	"""
+    :param height: La hauteur (nb de rangées)
+    :param width: La largeur (nb de colonnes)
+    :param data: Si une liste, alors les données elles-mêmes (affectées, pas copiées). Si un nombre, alors la valeur de remplissage
+    """
 
-	def __init__(self, height, width, data = 0.0):
-		if not isinstance(height, numbers.Integral) or not isinstance(width, numbers.Integral):
-			raise TypeError()
-		if height == 0 or width == 0:
-			raise ValueError(numbers.Integral)
-		self.__height = height
-		self.__width = width
-		if isinstance(data, list):
-			if len(data) != len(self):
-				raise ValueError(list)
-			self.__data = data
-		elif isinstance(data, numbers.Number):
-			self.__data = [data for i in range(len(self))]
-		else:
-			raise TypeError()
+    def __init__(self, height, width, data=0.0):
+        if not isinstance(height, numbers.Integral) or not isinstance(width, numbers.Integral):
+            raise TypeError()
+        if height == 0 or width == 0:
+            raise ValueError(numbers.Integral)
+        self.__height = height
+        self.__width = width
+        if isinstance(data, list):
+            if len(data) != len(self):
+                raise ValueError(list)
+            self.__data = data
+        elif isinstance(data, numbers.Number):
+            self.__data = [data for i in range(len(self))]
+        else:
+            raise TypeError()
 
-	@property
-	def height(self):
-		return self.__height
+    @property
+    def height(self):
+        return self.__height
 
-	@property
-	def width(self):
-		return self.__width
+    @property
+    def width(self):
+        return self.__width
 
-	@property
-	def data(self):
-		return self.__data
+    @property
+    def data(self):
+        return self.__data
 
-	# TODO: Accès à un élément en lecture
-	def TODO(TODO):
-		"""
-		Indexation rangée-major
+    # ATTENTION, quand on a un property, apres, on doit designer l'attribut sans le __ !!
+    # TODO: Accès à un élément en lecture
+    def __getitem__(self, indexes):
+        """
+        Indexation rangée-major
 
-		:param indexes: Les index en `tuple` (rangée, colonne)
-		"""
-		if not isinstance(indexes, tuple):
-			raise IndexError()
-		if indexes[0] >= self.height or indexes[1] >= self.width:
-			raise IndexError()
-		# TODO: Retourner la valeur
+        :param indexes: Les index en `tuple` (rangée, colonne)
+        """
+        if not isinstance(indexes, tuple):
+            raise IndexError()
+        if indexes[0] >= self.height or indexes[1] >= self.width:
+            raise IndexError()
 
-	# TODO: Affectation à un élément
-	def TODO(TODO):
-		"""
-		Indexation rangée-major
+        return self.data[indexes[0] * self.width + indexes[1]]
 
-		:param indexes: Les index en `tuple` (rangée, colonne)
-		"""
-		if not isinstance(indexes, tuple):
-			raise IndexError()
-		if indexes[0] >= self.height or indexes[1] >= self.width:
-			raise IndexError()
-		# TODO: L'affectation
+    # TODO: Retourner la valeur
 
-	def __len__(self):
-		"""
-		Nombre total d'éléments
-		"""
-		return self.height * self.width
+    # TODO: Affectation à un élément
+    def __setitem__(self, indexes, value: float):
+        """
+        Indexation rangée-major
 
-	# TODO: Représentation affichable (conversion pour print)
-	def TODO(TODO):
-		# TODO: Chaque rangée est sur une ligne, avec chaque élément séparé d'un espace.
-		pass
+        :param indexes: Les index en `tuple` (rangée, colonne)
+        """
+        if not isinstance(indexes, tuple):
+            raise IndexError()
+        if indexes[0] >= self.height or indexes[1] >= self.width:
+            raise IndexError()
 
-	# TODO: Représentation officielle
-	def TODO(TODO):
-		# TODO: une string qui représente une expression pour construire l'objet.
-		pass
+        n = 0
+        while n <= ((self.height * self.width) - 1):
+            if n == indexes[1]:
+                self.data[n] = value
+            self.data[n] = 0
 
-	# TODO: String formatée
-	def TODO(TODO):
-		# TODO: On veut pouvoir dir comment chaque élément doit être formaté en passant la spécification de formatage qu'on passerait à `format()`
-		pass
+    # TODO: L'affectation
 
-	def clone(self):
-		return Matrix(self.height, self.width, self.data)
+    def __len__(self):
+        """
+        Nombre total d'éléments
+        """
+        return self.height * self.width
 
-	def copy(self):
-		return Matrix(self.height, self.width, copy.deepcopy(self.data))
+    # TODO: Représentation affichable (conversion pour print)
+    def __str__(self):
+        # TODO: Chaque rangée est sur une ligne, avec chaque élément séparé d'un espace.
+        empty_str = ''
+        for n in self.data:
+            if self.data.index(n) == self.width - 1:
+                empty_str += f"{n}\n"
+            else:
+                empty_str += f"{n} "
 
-	def has_same_dimensions(self, other):
-		return (self.height, self.width) == (other.height, other.width)
+        print(empty_str)
 
-	def __pos__(self):
-		return self.copy()
+    # doesnt work
+    # TODO: Représentation officielle
+    def __repr__(self):
+        # TODO: une string qui représente une expression pour construire l'objet.
+        return self
 
-	# TODO: Négation
-	def TODO(TODO):
-		pass
+    # !!!
+    # TODO: String formatée
+    def TODO(TODO):
+        # TODO: On veut pouvoir dir comment chaque élément doit être formaté en passant la spécification de formatage qu'on passerait à `format()`
+        pass
 
-	# TODO: Addition
-	def TODO(TODO):
-		pass
-	
-	# TODO: Soustraction
-	def TODO(TODO):
-		pass
-	
-	# TODO: Multiplication matricielle/scalaire
-	def TODO(TODO):
-		if isinstance(other, Matrix):
-			# TODO: Multiplication matricielle.
-			# Rappel de l'algorithme simple pour C = A * B, où A, B sont matrices compatibles (hauteur_A = largeur_B)
-			# C = Matrice(hauteur_A, largeur_B)
-			# Pour i dans [0, hauteur_C[
-				# Pour j dans [0, largeur_C[
-					# Pour k dans [0, largeur_A[
-						# C(i, j) = A(i, k) * B(k, j)
-			pass
-		elif isinstance(other, numbers.Number):
-			# TODO: Multiplication scalaire.
-			pass
-		else:
-			raise TypeError()
+    def clone(self):
+        return Matrix(self.height, self.width, self.data)
 
-	# TODO: Multiplication scalaire avec le scalaire à gauche
+    def copy(self):
+        return Matrix(self.height, self.width, copy.deepcopy(self.data))
 
-	def __abs__(self):
-		return Matrix(self.height, self.width, [abs(e) for e in self.data])
+    def has_same_dimensions(self, other):
+        return (self.height, self.width) == (other.height, other.width)
 
-	# TODO: Égalité entre deux matrices
+    def __pos__(self):
+        return self.copy()
 
-	@classmethod
-	def identity(cls, width):
-		result = cls(width, width)
-		for i in range(width):
-			result[i, i] = 1.0
-		return result
+    # ATTENTION!! On doit retourner toute formule Matrix
+    # TODO: Négation
+    def __neg__(self):
+        for numb in self.data:
+            self.data[self.__data.index(numb)] = -1 * numb
+        return Matrix(self.height, self.width, self.data)
 
+    # TODO: Addition
+    def __add__(self, other):
+        if not self.has_same_dimensions(other):
+            raise ValueError(Matrix)
+        result = Matrix(self.height, self.width)
+        for i in range(len(self)):
+            result.data[i] = self.data[i] + other.data[i]
+        return result
+
+    # TODO: Soustraction
+    def __sub__(self, other):
+        for n in range(len(self.data)):
+            self.data[n] = self.data[n] - other[n]
+            return self + -other
+
+    # TODO: Multiplication matricielle/scalaire
+    def TODO(TODO):
+        if isinstance(other, Matrix):
+            # TODO: Multiplication matricielle.
+            # Rappel de l'algorithme simple pour C = A * B, où A, B sont matrices compatibles (hauteur_A = largeur_B)
+            # C = Matrice(hauteur_A, largeur_B)
+            # Pour i dans [0, hauteur_C[
+            # Pour j dans [0, largeur_C[
+            # Pour k dans [0, largeur_A[
+            # C(i, j) = A(i, k) * B(k, j)
+            pass
+        elif isinstance(other, numbers.Number):
+            # TODO: Multiplication scalaire.
+            pass
+        else:
+            raise TypeError()
+
+    # TODO: Multiplication scalaire avec le scalaire à gauche
+
+    def __abs__(self):
+        return Matrix(self.height, self.width, [abs(e) for e in self.data])
+
+    # TODO: Égalité entre deux matrices
+
+    @classmethod
+    def identity(cls, width):
+        result = cls(width, width)
+        for i in range(width):
+            result[i, i] = 1.0
+        return result
